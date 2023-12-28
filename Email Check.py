db11994ab2a20b41c5fecc1aceee0a703da6e539
Ravindra Kumar Gupta <140684982+ravindragupta7033@users.noreply.github.com>
2023-12-28 21:20:13 +0530

    Add files via upload

---
+++ Email Check.py
@@ -0,0 +1,13 @@
+import re
+email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w{9}$+[.]\w{2,3}$"
+
+Your_Email=input("Enter your Email:")
+if "gmail" in Your_Email:
+    if re.search(email_condition,Your_Email):
+        print("Your Email  is Valid 👍🏿👍🏾")
+    else:
+        print("Sorry This Invalid Email ❌‍💻🫥👎2 ")
+
+else:
+    print("Sorry This Invalid Email ❌‍💻🫥👎1 ")
+
